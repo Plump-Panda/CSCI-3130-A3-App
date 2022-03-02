7bc67c7ac256c10ee351e3b2d045f9dec77feef7
Joshua Fung <formygit@gmail.com>
2022-03-02 13:21:33 -0800

    Q5: Created 2 new methods with extract technique

--- app/src/main/java/ca/dal/cs/csci3130/a3/MainActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/a3/MainActivity.java
@@ -16,30 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button factorialButton = findViewById(R.id.factorialButton);
-        factorialButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                EditText numberBox = findViewById(R.id.numberBox);
-                int number = Integer.parseInt(numberBox.getText().toString());
-                int factorial = calculateFactorial(number);
-                TextView statusLabel = findViewById(R.id.result);
-                statusLabel.setText(String.valueOf(factorial));
-            }
-        });
+        buttonListenerSetup(factorialButton, true);
 
         Button cubeRootButton = findViewById(R.id.cubeRootButton);
-        cubeRootButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                EditText numberBox = findViewById(R.id.numberBox);
-                int number = Integer.parseInt(numberBox.getText().toString());
-                int cubeRoot = calculateCubeRoot(number);
-                TextView statusLabel = findViewById(R.id.result);
-                statusLabel.setText(String.valueOf(cubeRoot));
-            }
-        });
+        buttonListenerSetup(cubeRootButton, false);
     }
 
 
@@ -49,6 +29,29 @@ public class MainActivity extends AppCompatActivity {
             factorial = factorial * i;
         }
         return factorial;
+    }
+
+    protected void calculateAnswer(boolean isFactorial){
+        EditText numberBox = findViewById(R.id.numberBox);
+        int number = Integer.parseInt(numberBox.getText().toString());
+        int answer;
+        if(isFactorial){
+            answer = calculateFactorial(number);
+        }else{
+            answer = calculateCubeRoot(number);
+        }
+        TextView statusLabel = findViewById(R.id.result);
+        statusLabel.setText(String.valueOf(answer));
+    }
+
+    protected void buttonListenerSetup(Button button, boolean isFactorial){
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                calculateAnswer(isFactorial);
+            }
+        });
     }
 
     protected int calculateCubeRoot(int number) {
